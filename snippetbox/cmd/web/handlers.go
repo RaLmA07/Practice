1ded839763decd2f70688aa808cbc7c9356cb823
RaLmA07 <dauesovr@gmail.com>
2024-06-04 12:26:31 +0500

    7

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -14,7 +14,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
@@ -27,7 +33,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal Server Error", 500)
 	}
 
-	w.Write([]byte("Привет из Snippetbox"))
+	//w.Write([]byte("Привет из Snippetbox"))
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
